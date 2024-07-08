dcf27915a88894b5ccf9a86558da9b9ef727e01d
Tom De Moor <tom@controlaltdieliet.be>
2024-07-08 17:23:08 +0000

    daily update

--- scripts/main.go
+++ scripts/main.go
@@ -3,7 +3,7 @@ package main
 const (
 	layout              = "2006-1-2"
 	pathScript          = "/home/tom/mattermost-i18n-scoreboard/"
-	weblateToken string = ""
+	weblateToken string = "jn8fCVl8oSRZ9mei7iUopftK4g3s2uZAIccL66PC"
 )
 
 func main() {
